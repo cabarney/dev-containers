d9c7cbdcfc5f8e6dd0c0b77c7bf9b113e8ec994e
Barney, Adam <adambarney@rocketmortgage.com>
2022-10-25 13:43:24 -0500

    env files, pre-built images

--- demo/final/dotnet7/Program.cs
+++ demo/final/dotnet7/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Indy!");
+app.MapGet("/", (IConfiguration config) => $"Hello {config["name"]}!");
 
 app.Run();
